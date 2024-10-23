f2fb56cc19cad2e8df8acbed9ad4bb8a93391540
Isabelcerons <125101862+Isabelcerons@users.noreply.github.com>
2024-10-23 17:56:18 -0500

    parcial2

---
+++ build/types/parcialBackEnd.js
@@ -0,0 +1,9 @@
+console.log('\n 7. Uso de ciclos');
+let sum = 0;
+for (let i = 1; i <= 100; i++) {
+    if (i % 2 === 0) {
+        sum += i;
+    }
+}
+console.log(`\n 7.1 Suma de números pares: ${sum}`);
+//# sourceMappingURL=parcialBackEnd.js.map
